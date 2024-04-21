014b1c8b705740a687e7386dd751cdde00ecc451
Kevin Egstorf <kevin.egstorf@gmail.com>
2024-04-21 16:33:19 +0200

    first commit

--- src/app/brew-steps/brew-steps.component.ts
+++ src/app/brew-steps/brew-steps.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-brew-steps',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './brew-steps.component.html',
-  styleUrl: './brew-steps.component.scss'
+  styleUrl: './brew-steps.component.scss',
 })
 export class BrewStepsComponent {
+  constructor(private router: Router) {}
 
+  handleResetChecklist() {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate(['/brouw-stappen']);
+  }
 }
